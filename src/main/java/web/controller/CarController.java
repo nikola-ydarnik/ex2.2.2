0d88d952902557a9d9f1c1042b8aa1601d04f205
nikola-ydarnik <nikola-ydarnik@bk.ru>
2024-02-27 19:38:44 +0300

    Исправил задание: Хорошо, но давай объединим getCars и getCarsByAmount. Они делают то же самое в сущности

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
 import web.service.CarServiceImpl;
 
@@ -12,15 +13,26 @@ public class CarController {
 
     private final CarService carService = new CarServiceImpl();
 
-    @GetMapping("/cars")
-    public String getCars(Model model) {
-        model.addAttribute("cars", carService.getAllCars());
-        return "show_all_cars";
-    }
+//    @GetMapping("/cars")
+//    public String getCars(Model model) {
+//        model.addAttribute("cars", carService.getAllCars());
+//        return "show_all_cars";
+//    }
+//
+//    @GetMapping("/cars/{count}")
+//    public String getCarsByAmount(@PathVariable("count") int count, Model model) {
+//        model.addAttribute("cars", carService.getCarsByAmount(count));
+//        return "show_cars_by_amount";
+//    }
 
-    @GetMapping("/cars/{count}")
-    public String getCarsByAmount(@PathVariable("count") int count, Model model) {
-        model.addAttribute("cars", carService.getCarsByAmount(count));
-        return "show_cars_by_amount";
+//    Хорошо, но давай объединим getCars и getCarsByAmount. Они делают то же самое в сущности
+    @GetMapping("/cars")
+    public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
+        if (count == null) {
+            model.addAttribute("cars", carService.getAllCars());
+        } else {
+            model.addAttribute("cars", carService.getCarsByAmount(count));
+        }
+        return "cars";
     }
 }
